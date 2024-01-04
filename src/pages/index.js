4492304aff26def3563fa6e5029e0176b06e973f
Mao-Lin Wang <focaaby@gmail.com>
2024-01-04 10:36:47 +0800

    Remove: description in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,8 +25,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}>
       <HomepageHeader />
       <main>
         {/* <HomepageFeatures /> */}
